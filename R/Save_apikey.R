d13dae5927daf3fb211eef535b2d2d120364678a
Coskun Erden <cerdentr@gmail.com>
2022-05-05 13:01:43 -0500

    save_apikey function edited

--- R/Save_apikey.R
+++ R/Save_apikey.R
@@ -7,7 +7,26 @@
 #'
 #' @export
 
-Save_apikey <- function(apikey = NULL) {
+save_apikey <- function(apikey = NULL) {
   usethis::edit_r_environ('project')
-  apikey = apikey
+  assertthat::assert_that(file.exists(".Renviron"))
+  renv_contents <- readLines(".Renviron")
+  assertthat::assert_that(!is.null(apikey))
+  file_changed <- F
+  
+  if (!is.null(apikey)) {
+    assertthat::assert_that(is.character(apikey))
+    apikey_exists <- any(grepl("apikey", renv_contents))
+    if (!apikey_exists) {
+      str <- sprintf('\napikey = \"%s\"\n', apikey)
+      cat(str, file = ".Renviron", append = TRUE)
+      file_changed <- T
+    } else {
+      warning(".Renviron already contains an apikey; no file change was made.\n")
+    }
+  }
+  
+  if (file_changed) {
+    source(".Renviron")
+  }
 }
