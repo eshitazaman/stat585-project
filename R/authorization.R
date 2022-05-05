739cb691453b9de6aaf162d9db9e7e2899538993
Coskun Erden <cerdentr@gmail.com>
2022-05-05 13:46:22 -0500

    minor changes

--- R/authorization.R
+++ R/authorization.R
@@ -14,37 +14,11 @@ newsapi_setup <- function() {
 }
 
 
-#### Function for saving apikey to Rprofile
+#### Function for saving apikey to .Renviron
 
-newsapi_save_apikey <- function(apikey = NULL) {
-  assertthat::assert_that(file.exists(".Rprofile"))
-  rprof_contents <- readLines(".Rprofile")
-  assertthat::assert_that(!is.null(apikey))
-  file_changed <- F
-  
-  if (!is.null(apikey)) {
-    assertthat::assert_that(is.character(apikey))
-    apikey_exists <- any(grepl("newsapi_apikey", rprof_contents))
-    if (!apikey_exists) {
-      str <- sprintf('\nnewsapi_apikey = \"%s\"\n', apikey)
-      cat(str, file = ".Rprofile", append = TRUE)
-      file_changed <- T
-    } else {
-      warning(".Rprofile already contains an apikey; no file change was made.\n")
-    }
-  }
-  
-  if (file_changed) {
-    source(".Rprofile")
-  }
-  #     if (!requireNamespace("rstudioapi", quietly = T) || 
-  #         !requireNamespace("fs", quietly = T)) {
-  # 
-  #       message("Your credentials will be available in your Global Environment after restarting RStudio.")
-  #     } else {
-  #       restart_rstudio("Your credentials will be available in your Global Environment after restarting RStudio.")
-  #     }
-  #   }
+Save_apikey <- function(apikey = NULL) {
+  usethis::edit_r_environ('project')
+  apikey = apikey
 }
 
 
